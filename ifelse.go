7cc75f1d7fbc3c666a34d29e2c415ec9c1510cb5
Aman17101 <127509841+Aman17101@users.noreply.github.com>
2023-10-23 16:51:46 +0530

    Create ifelse.go

---
+++ ifelse.go
@@ -0,0 +1,16 @@
+package main
+
+
+import "fmt"
+func main(){
+	var x int=123
+	if(x==19){
+	fmt.Printf("x is less than 100\n")
+}else if(x==123){
+	fmt.Printf("x is equal")
+	}else{
+	fmt.Printf("Aman thakur ")
+	}
+
+
+}
