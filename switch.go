74b2fb7e7f5c1ac544b4309353f752e0c9f0e675
Aman17101 <127509841+Aman17101@users.noreply.github.com>
2023-10-26 14:17:30 +0530

    Create switch.go

---
+++ switch.go
@@ -0,0 +1,20 @@
+package main
+import  "fmt"
+func main(){
+	var g string="c"
+	switch{
+	case g=="a":
+		fmt.Printf("good grade")
+	
+case g=="b":
+	fmt.Printf("avg grade")
+
+case g=="c":
+	fmt.Printf("not good grade")
+
+case g=="d":
+	fmt.Printf("failed")
+
+
+}
+}
